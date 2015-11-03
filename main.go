f437a7e22dfad8324c6105c50cb7dce2e5693b1b
SeongJae Park <sj38.park@gmail.com>
2015-11-04 03:06:56 +0900

    implement ugly outyet
    Now, the webserver says whether go1.6 is out or not yet.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 )
 
 func handleHttp(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World")
+	ret, err := http.Head("https://go.googlesource.com/go/+/go1.6")
+	if err != nil {
+		log.Print(err)
+		io.WriteString(w, "error from http.Head()")
+	}
+	if ret.StatusCode != http.StatusOK {
+		io.WriteString(w, "Go 1.6 is not out yet...")
+		return
+	}
+	io.WriteString(w, "Go 1.6 is out!")
 }
 
 func main() {
